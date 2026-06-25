a1f8fa35ca57fed88bc9ae94b6dd96f013c4c8b8
Alex Barks <abarks@g.clemson.edu>
2026-06-24 23:46:34 -0400

    supabase updating

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,7 +2,7 @@ class AppConfig {
   static const primaryColor = 0xFF6BD425;   // light green
   static const secondaryColor = 0xFF4DB6E2; // light blue
   static const appName = "ReadRight";
-  static const supabaseUrl = 'https://byhmgdgjlyphwyilrfjm.supabase.co';
+  static const supabaseUrl = 'https://rafzvinelnfdlwzqtqbc.supabase.co';
   static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5aG1nZGdqbHlwaHd5aWxyZmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4NDU1NDksImV4cCI6MjA3NzQyMTU0OX0.rkxUJIWYpoPpCV3azuK7vwenPATJeLjzTdTn13savZM';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhZnp2aW5lbG5mZGx3enF0cWJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMzgzOTcsImV4cCI6MjA5NzkxNDM5N30.iqxKHTS82CfOdUGOmTtM1BkUhbboFR_AOlE7FNaqGBM';
 }
